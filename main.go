9dc54c7f0ee4c8f69fead868463dd03cae6a4422
Mykola Martynov <mykola.martynov@hotmail.com>
2024-07-04 01:37:57 +0300

    Stage 5

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"log"
 	"os"
+	"slices"
 	"sort"
+	"strconv"
+	"strings"
 )
 
 const NumDepartmentsPerApplicant = 3
@@ -16,15 +20,39 @@ type Person struct {
 }
 
 type Department string
+type Subject int
+
+const (
+	physics Subject = iota
+	chemistry
+	math
+	computerScience
+)
 
 type Applicant struct {
 	Person
-	Deps []Department
-	GPA  float64
+	ExamScores map[Subject]float64
+	Deps       []Department
+	GPA        float64
 }
 
 func (p *Person) Fullname() string {
 	return p.Firstname + " " + p.Lastname
+}
+
+func (a *Applicant) GetScore(dep Department) float64 {
+	switch dep {
+	case "Physics":
+		return a.ExamScores[physics]
+	case "Chemistry", "Biotech":
+		return a.ExamScores[chemistry]
+	case "Mathematics":
+		return a.ExamScores[math]
+	case "Engineering":
+		return a.ExamScores[computerScience]
+	default:
+		panic("Unknown department")
+	}
 }
 
 func main() {
@@ -33,14 +61,14 @@ func main() {
 	var maxApplicants int
 	fmt.Scan(&maxApplicants)
 
-	applicantsByDepartment := distributeApplicants(applicants, maxApplicants)
-	departments := getDepartments(applicantsByDepartment)
+	departments := getDepartments(applicants)
+	applicantsByDepartment := distributeApplicants(departments, applicants, maxApplicants)
 
 	// display result
 	for _, department := range departments {
 		fmt.Println(department)
-		for _, applicant := range *applicantsByDepartment[department] {
-			fmt.Printf("%s %.2f\n", applicant.Fullname(), applicant.GPA)
+		for _, applicant := range applicantsByDepartment[department] {
+			fmt.Printf("%s %.2f\n", applicant.Fullname(), applicant.GetScore(department))
 		}
 		fmt.Println()
 	}
@@ -57,59 +85,107 @@ func loadInfo() (applicants []Applicant) {
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		line := scanner.Text()
-		var firstname, lastname string
-		var dep1, dep2, dep3 Department
-		var gpa float64
-
-		// fetch applicant info
-		fmt.Sscan(line, &firstname, &lastname, &gpa, &dep1, &dep2, &dep3)
-
-		applicant := Applicant{
-			Person{firstname, lastname},
-			[]Department{dep1, dep2, dep3},
-			gpa,
+		if applicant, err := getApplicant(strings.Split(scanner.Text(), " ")); err == nil {
+			applicants = append(applicants, applicant)
 		}
-
-		applicants = append(applicants, applicant)
 	}
 
 	return
 }
 
-func distributeApplicants(applicants []Applicant, maxApplicants int) map[Department]*[]Applicant {
-	departments := make(map[Department]*[]Applicant, NumDepartmentsPerApplicant)
+func getApplicant(s []string) (applicant Applicant, err error) {
+	if len(s) != 9 {
+		err = errors.New("Invalid line")
+		return
+	}
 
-	sortApplicantsByGPA(&applicants)
+	applicant.Firstname = s[0]
+	applicant.Lastname = s[1]
+	applicant.ExamScores = make(map[Subject]float64, 4)
+	applicant.ExamScores[physics], _ = strconv.ParseFloat(s[2], 64)
+	applicant.ExamScores[chemistry], _ = strconv.ParseFloat(s[3], 64)
+	applicant.ExamScores[math], _ = strconv.ParseFloat(s[4], 64)
+	applicant.ExamScores[computerScience], _ = strconv.ParseFloat(s[5], 64)
+	applicant.Deps = make([]Department, 0, 3)
+	applicant.Deps = append(applicant.Deps, Department(s[6]))
+	applicant.Deps = append(applicant.Deps, Department(s[7]))
+	applicant.Deps = append(applicant.Deps, Department(s[8]))
+
+	gpa := applicant.ExamScores[physics]
+	gpa += applicant.ExamScores[chemistry]
+	gpa += applicant.ExamScores[math]
+	gpa += applicant.ExamScores[computerScience]
+
+	applicant.GPA = gpa / 4
+
+	return
+}
+
+func distributeApplicants(departments []Department, applicants []Applicant, maxApplicants int) map[Department][]Applicant {
+	deps := make(map[Department][]Applicant, len(departments))
 
 	for i := 0; i < NumDepartmentsPerApplicant; i++ {
-		for k, j := 0, len(applicants); j > 0; j-- {
-			applicant := applicants[k]
-			dep := applicant.Deps[i]
-			if nil == departments[dep] {
-				departments[dep] = new([]Applicant)
-			}
-			if len(*departments[dep]) < maxApplicants {
-				*departments[dep] = append(*departments[dep], applicant)
-				applicants = append(applicants[:k], applicants[k+1:]...)
-			} else {
-				k++
-			}
+		for _, dep := range departments {
+			deps[dep] = append(deps[dep], selectApplicantsByDepartment(dep, &applicants, i, maxApplicants-len(deps[dep]))...)
 		}
 	}
 
-	// sort applicants by GPA for each department
-	for dep := range departments {
-		sortApplicantsByGPA(departments[dep])
+	for dep, selectedApplicants := range deps {
+		sortApplicantsByDepartment(dep, &selectedApplicants)
+		deps[dep] = selectedApplicants
 	}
 
-	return departments
+	return deps
 }
 
-func getDepartments(applicantsByDepartment map[Department]*[]Applicant) []Department {
-	departments := make([]Department, 0, len(applicantsByDepartment))
-	for dep := range applicantsByDepartment {
-		departments = append(departments, dep)
+func selectApplicantsByDepartment(department Department, applicants *[]Applicant, priority, maxApplicants int) []Applicant {
+	if maxApplicants <= 0 {
+		return nil
+	}
+
+	selectedApplicants := make([]Applicant, 0, maxApplicants)
+
+	sortApplicantsByDepartment(department, applicants)
+
+	for k, j := 0, len(*applicants); j > 0; j-- {
+		applicant := (*applicants)[k]
+		dep := applicant.Deps[priority]
+		if dep != department {
+			k++
+			continue
+		}
+
+		selectedApplicants = append(selectedApplicants, applicant)
+		*applicants = append((*applicants)[:k], (*applicants)[k+1:]...)
+
+		if len(selectedApplicants) == maxApplicants {
+			break
+		}
+	}
+
+	return selectedApplicants
+}
+
+func sortApplicantsByDepartment(department Department, applicants *[]Applicant) {
+	sort.Slice(*applicants, func(i, j int) bool {
+		a, b := (*applicants)[i], (*applicants)[j]
+		score1, score2 := a.GetScore(department), b.GetScore(department)
+		if score1 != score2 {
+			return score1 > score2
+		}
+		return a.Fullname() < b.Fullname()
+	})
+}
+
+func getDepartments(applicants []Applicant) []Department {
+	departments := make([]Department, 0, 4)
+
+	for _, applicant := range applicants {
+		for _, dep := range applicant.Deps {
+			if !slices.Contains(departments, dep) {
+				departments = append(departments, dep)
+			}
+		}
 	}
 
 	sort.Slice(departments, func(i, j int) bool {
@@ -117,14 +193,4 @@ func getDepartments(applicantsByDepartment map[Department]*[]Applicant) []Depart
 	})
 
 	return departments
-}
-
-func sortApplicantsByGPA(applicants *[]Applicant) {
-	sort.Slice(*applicants, func(i, j int) bool {
-		a, b := (*applicants)[i], (*applicants)[j]
-		if a.GPA != b.GPA {
-			return a.GPA > b.GPA
-		}
-		return a.Fullname() < b.Fullname()
-	})
 }
